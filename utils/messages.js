649bbf7454f9db411eb538a5ca843ca92296a61c
Bhawani Shankar <bhaweshlohar2@gmail.com>
2021-12-26 17:30:56 +0530

    local date updated

--- utils/messages.js
+++ utils/messages.js
@@ -1,16 +1,11 @@
+const moment = require('moment');
 
-const d = new Date();
-let hour =
-     ((d.getHours() + 5) === 12 ? '12' : (d.getHours()+5) % 12) +
-    ":" +
-    (d.getMinutes() + 30) +
-    ` ${(d.getHours() + 5) >= 12 ? "pm" : "am"}`;
 
 function formatMessage(username, text) {
     return {
         username,
         text,
-        time: hour
+        time: moment().format('h:mm a')
     }
 }
 
