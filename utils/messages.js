4a7854123a9f3d651c5652c334138080564fbae4
Bhawani Shankar <bhaweshlohar2@gmail.com>
2021-12-26 17:04:44 +0530

    change time according to utc time

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,10 @@
 
 const d = new Date();
 let hour =
-     (d.getHours() === 12 ? '12' : (d.getHours() % 12)) +
+     ((d.getHours() + 5) === 12 ? '12' : (d.getHours()+5) % 12) +
     ":" +
-    d.getMinutes() +
-    ` ${d.getHours() >= 12 ? "pm" : "am"}`;
+    (d.getMinutes() + 30) +
+    ` ${(d.getHours() + 5) >= 12 ? "pm" : "am"}`;
 
 console.log("timeee", hour);
 function formatMessage(username, text) {
