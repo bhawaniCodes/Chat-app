0519bccb5ba18924ba9177b4e454afc5d40d9366
Bhawani Shankar <bhaweshlohar2@gmail.com>
2021-12-26 10:31:12 +0530

    final commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,7 +3,7 @@ let sendButton = document.getElementById("send-button");
 let displayMessage = document.getElementById("chat-message-display");
 let addUserDiv = document.getElementById("add-user-here");
 let channelNameP = document.getElementById("channel-name");
-const socket = io("https://afternoon-hamlet-53999.herokuapp.com/", {
+const socket = io("http://localhost:3000/", {
     transports: ["websocket"],
 });
 
